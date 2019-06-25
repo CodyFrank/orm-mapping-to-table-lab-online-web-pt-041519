7d2f4061f48dd9efeb4f70ce7ea227d4c52dc4e3
Cody Frank <cody.frank30@gmail.com>
2019-06-25 00:44:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,19 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
+
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE students(
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    grade INTEGER
+    )
+    SQL
+    DB[:conn].ececute(sql)
+  end
+
+  
   def initialize(name, grade, id = nil)
     @name = name
     @id = id
