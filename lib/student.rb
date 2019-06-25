0519744c2fa2ae31d7ec2265cd0952f5fd8abaa5
Cody Frank <cody.frank30@gmail.com>
2019-06-25 00:48:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,14 @@ attr_reader :id
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
-    )
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students;
     SQL
     DB[:conn].execute(sql)
   end
